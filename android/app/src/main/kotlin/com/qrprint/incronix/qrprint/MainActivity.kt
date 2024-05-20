a2e2dc78c88532a01956eea023a82a43bb1e9fa0
Nimish <nimish.incronix@gmail.com>
2024-05-20 11:36:32 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/qrprint/incronix/qrprint/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qrprint.incronix.qrprint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/qr_scanner.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QrPrint extends StatefulWidget {
+  const QrPrint({super.key});
+
+  @override
+  State<QrPrint> createState() => _QrPrintState();
+}
+
+class _QrPrintState extends State<QrPrint> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
